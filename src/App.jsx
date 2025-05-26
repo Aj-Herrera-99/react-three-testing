80fac9195d325650bc801fbcc9d5a2743b62f07d
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-05-26 22:49:57 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Scene from './components/Scene'
 function App() {
 
   return (
-    <main className='relative h-screen'>
+    <main className='relative h-[100dvh]'>
       <Scene />
     </main>
   )
